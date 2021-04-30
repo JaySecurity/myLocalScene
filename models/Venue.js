cf4e501ad0c7d34eff77102a5830399ea7093063
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-04-30 17:03:58 -0400

    add Venue Model

--- models/Venue.js
+++ models/Venue.js
@@ -2,3 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = require('./review');
+
+const venueSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    name: String,
+    phone: String,
+    email: String,
+  },
+  website: String,
+  reviews: [reviewSchema],
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+module.exports = mongoose.model('Venue', venueSchema);
