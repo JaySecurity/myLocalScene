4f349f3db5b324184b4568c7d53bed63fac0b62c
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-04-30 16:45:56 -0400

    add User Model and review Schema

---
+++ models/review.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.exports = ReviewSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
